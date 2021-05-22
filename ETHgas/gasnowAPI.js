b5ac78504f4cfebd74e87fc952c8aff7ad1317c6
RustyGray0 <abhi.chulani@gmail.com>
2021-05-22 19:46:52 -0400

    added automatic refresh

--- ETHgas/gasnowAPI.js
+++ ETHgas/gasnowAPI.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
+function refresh(){
   $.ajax({
     url:  "https://www.gasnow.org/api/v3/gas/price?utm_source=:test",
     type: "GET",
     success: function(result){
+      $(".gasPrice").children().not(".gasText").html("");
       $("<span/>").text(Math.round(result.data.rapid/1000000000)).appendTo($("#gasRapid"));
       $("<span/>").text(Math.round(result.data.fast/1000000000)).appendTo($("#gasFast"));
       $("<span/>").text(Math.round(result.data.standard/1000000000)).appendTo($("#gasStandard"));
@@ -12,4 +13,8 @@ $(document).ready(function(){
       console.log(error);
     }
   });
-});
+}
+
+refresh()
+
+setInterval(function(){ refresh() }, 10000);

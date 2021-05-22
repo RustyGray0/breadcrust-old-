58ea194eaf36c5cfa1b6f235d6e8b4b0d420699c
RustyGray0 <abhi.chulani@gmail.com>
2021-05-22 18:21:08 -0400

    added more css

--- ETHgas/gasnowAPI.js
+++ ETHgas/gasnowAPI.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     url:  "https://www.gasnow.org/api/v3/gas/price?utm_source=:test",
     type: "GET",
     success: function(result){
-      $("<li/>").text("rapid: "+result.data.rapid/1000000000).appendTo($("#gasPrice"));
-      $("<li/>").text("fast: "+result.data.fast/1000000000).appendTo($("#gasPrice"));
-      $("<li/>").text("standard: "+result.data.standard/1000000000).appendTo($("#gasPrice"));
-      $("<li/>").text("slow: "+result.data.slow/1000000000).appendTo($("#gasPrice"));
+      $("<span/>").text(Math.round(result.data.rapid/1000000000)).appendTo($("#gasRapid"));
+      $("<span/>").text(Math.round(result.data.fast/1000000000)).appendTo($("#gasFast"));
+      $("<span/>").text(Math.round(result.data.standard/1000000000)).appendTo($("#gasStandard"));
+      $("<span/>").text(Math.round(result.data.slow/1000000000)).appendTo($("#gasSlow"));
     },
     error: function(error){
       console.log(error);
